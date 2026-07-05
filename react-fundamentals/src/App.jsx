d400f4e75e4d6a4046a6796eebec6c3073e875fd
Retiishia <azizfarhan72@gmail.com>
2026-07-06 01:07:48 +0700

    feat(day 3): learn reusable components and props

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -2,23 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Greeting from './greeting.jsx'
 import './App.css'
 
 function App() {
-  const name = "Retiishia";
-  const user = {
-    firstName: "Farhan",
-    lastName: "Aziz",
-    age: 25
-  }
-
   return (
     <>
-    <h1>Hello {user.firstName} {user.lastName}</h1>
-    <h2>{user.age}</h2>
-    <h3>{Math.random()}</h3>
-    <h3>{new Date().getFullYear()}</h3>
-    </>
+    <Greeting name="Farhan"/>
+    <Greeting name="Aziz"/>
+    <Greeting name="Retiishia"/>
+</>
+   
   )
 }
 
---
+++ react-fundamentals/src/Greeting.jsx
@@ -0,0 +1,6 @@
+function Greeting({name}) {
+  console.log({name})
+  return <h1>Hello {name}</h1>;
+}
+
+export default Greeting;
