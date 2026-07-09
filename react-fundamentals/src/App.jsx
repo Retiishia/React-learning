0e8f265dbcb8742e6f1046b3adcbd784c2811478
Retiishia <azizfarhan72@gmail.com>
2026-07-10 02:16:24 +0700

    feat(day-4 & 5): implement reusable Card with props and children

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -3,16 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import Greeting from './greeting.jsx'
+import Card from './Card.jsx'
 import './App.css'
 
 function App() {
   return (
     <>
-    <Greeting name="Farhan"/>
-    <Greeting name="Aziz"/>
-    <Greeting name="Retiishia"/>
+    <Card title="React Study Plan">
+      <p>Day 5 - Children Props</p>
+    </Card>
+
+    <Card title="Expense Tracker">
+      <p>JavaScript Project</p>
+    </Card>
+
+    <Card title="Weather App">
+      <p>Using OpenWeather API</p>
+    </Card>
 </>
-   
   )
 }
 
