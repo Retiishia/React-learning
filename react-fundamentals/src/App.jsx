c7e3e652556675d8a545bb939ccb8277a9dae1e7
Retiishia <azizfarhan72@gmail.com>
2026-08-04 03:30:52 +0700

    Add login form

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -1,19 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import Greeting from './greeting.jsx'
 import './App.css'
 
 function App() {
-  return (
-    <>
-    <Greeting name="Farhan"/>
-    <Greeting name="Aziz"/>
-    <Greeting name="Retiishia"/>
-</>
-   
-  )
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value)
+    }
+
+    function handleLogin(event) {
+        event.preventDefault()
+        console.log('Username:', username)
+        console.log('Password:', password)
+    }
+
+    return (
+        <form onSubmit={handleLogin}>
+            <h2>Login Form</h2>
+            <div>
+                <label>Username</label>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={handleUsernameChange}
+                />
+            </div>
+            <div>
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                />
+            </div>
+            <button type="submit" onClick={handleLogin}>
+                Login
+            </button>
+        </form>
+    )
 }
 
-export default App;
+export default App
